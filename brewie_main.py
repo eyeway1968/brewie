f1c65946e9485fc0e2f06c08624638d8828a2357
eyeway1968 <eyeway12@otenet.gr>
2020-03-02 23:44:38 +0200

    modifications

--- brewie_main.py
+++ brewie_main.py
@@ -2,38 +2,42 @@
 import sys
  
 # This gets the Qt stuff
-#import PyQt5
-from PyQt5 import QtWidgets, uic 
+import PyQt5
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
  
 # This is our window from QtCreator
 
  
 # create class for our Raspberry Pi GUI
-class Ui(QtWidgets.QMainWindow):
+class Ui(QMainWindow):
  # access variables inside of the UI's file
  
  ### functions for the buttons to call
  
- 
+ def PressedBtn(self):
+  print("button pressed")
+
+
  def __init__(self):
-  super(Ui, self).__init__() # Call the inherited classes __init__ method
+  super(self.__class__, self).__init__() # Call the inherited classes __init__ method
   uic.loadUi('brewie.ui', self) # Load the .ui file
-  self.show() # Show the GUI
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+  
+  self.btn1.clicked.connect(lambda: self.PressedBtn)
+  
+  
+  #self.show() # Show the GUI
+
+
+ 
+
+app = QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
 window = Ui() # Create an instance of our class
+window.show()
 app.exec_() # Start the application 
  ### Hooks to for buttons
-  
-# def main():
-#  # a new app instance
-#  app = QtWidgets.QApplication(sys.argv)
-#  form = Ui()
-#  form.show()
-#  # without this, the script exits immediately.
-#  sys.exit(app.exec_())
+
+
+
  
-# # python bit to figure how who started This
-# if __name__ == "__main__":
-#  main()
- ### functions for the buttons to call
