0bbfd127d836c70eb0477eff4505e001754baf28
george panagopoulos <info@eyeway.gr>
2020-03-06 20:56:43 +0200

    multi window attempt

--- brewie_main.py
+++ brewie_main.py
@@ -4,9 +4,9 @@ import sys
 # This gets the Qt stuff
 import PyQt5
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+#from PyQt5 import uic
+from PyQt5.uic import loadUi
 
- 
 # This is our window from QtCreator
 # create class for our Raspberry Pi GUI
 class Ui(QMainWindow):
@@ -18,22 +18,32 @@ class Ui(QMainWindow):
   print("button pressed")
 
 
- def __init__(self, parent=None):
-  super(self.__class__, self).__init__(parent) # Call the inherited classes __init__ method
-  uic.loadUi('brewie.ui', self) # Load the .ui file
+ def __init__(self):
+  super(self.__class__, self).__init__() # Call the inherited classes __init__ method
+  loadUi('brewie.ui', self) # Load the .ui file
   
-  self.btn1.clicked.connect(lambda:setCurrentIndex(self))
+  self.btn1.clicked.connect(lambda:openOtherForm(self))
   self.dial.valueChanged.connect(lambda: setCurrentIndex(self))
-
+  
   def setCurrentIndex(self):
     currentIndex = self.dial.value()
     self.label.setText(str(currentIndex))
-    
+  def openOtherForm(self):
+        self.hide()
+        otherview = SecondWindow(self)
+        otherview.show()  
 
   #self.show() # Show the GUI
 
+class SecondWindow(QDialog):
+  def __init__(self, parent = None):
+    super(SecondWindow, self).__init__(parent)
+    loadUi('second.ui', self)
+    self.button2.clicked.connect(self.goBackToOtherForm)
+  def goBackToOtherForm(self):
+        self.parent().show()
+        self.close()
 
- 
 def main():
  app = QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
  window = Ui() # Create an instance of our class
