60807e8b00eb4683b5248d1ad6c1b2e1dddfd6f1
eyeway1968 <eyeway12@otenet.gr>
2020-03-07 21:07:44 +0200

    changes

--- brewie_main.py
+++ brewie_main.py
@@ -1,15 +1,11 @@
 # always seem to need this
 import sys
- 
-# This gets the Qt stuff
-import PyQt5
-from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 
- 
 # This is our window from QtCreator
 # create class for our Raspberry Pi GUI
-class Ui(QMainWindow):
+class Ui(QtWidgets.QMainWindow):
  # access variables inside of the UI's file
  
  ### functions for the buttons to call
@@ -18,29 +14,25 @@ class Ui(QMainWindow):
   print("button pressed")
 
 
- def __init__(self, parent=None):
-  super(self.__class__, self).__init__(parent) # Call the inherited classes __init__ method
+ def __init__(self, *args, **kwargs):
+  super().__init__(*args, **kwargs) # Call the inherited classes __init__ method
   uic.loadUi('brewie.ui', self) # Load the .ui file
   
-  self.btn1.clicked.connect(lambda:setCurrentIndex(self))
+  
   self.dial.valueChanged.connect(lambda: setCurrentIndex(self))
-
+  
   def setCurrentIndex(self):
     currentIndex = self.dial.value()
     self.label.setText(str(currentIndex))
-    
+  
 
   #self.show() # Show the GUI
 
 
  
 
-app = QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
 window = Ui() # Create an instance of our class
 window.show()
 app.exec_() # Start the application 
  ### Hooks to for buttons
-
-
-
- 
